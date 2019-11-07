4538db6330b65c7edb57b7ec49daabf0e1690c64
Liliwang221 <55935866+Liliwang221@users.noreply.github.com>
2019-11-07 19:40:28 +0800

    Initial commit

---
+++ server.js
@@ -0,0 +1,18 @@
+const http=require('http');
+const fs=require('fs');
+const server=http.createServer((req,res)=>{
+    if(req.url==='/txt'){
+        res.writeHead(200,{'Content-Type':'text/plain'})
+        res.end('txt')
+    }else if(req.url==='/json'){
+        res.writeHead(200,{'Content-Type':'application/json'})
+        res.end(JSON.stringify({code:1,list:[1,2,3]}))
+    }else if(req.url==='/jpg'){
+        res.writeHead(200,{'Content-Type':'image/jpeg'})
+        let imgBuf=fs.readFileSync('./1.jpg')
+        res.end(imgBuf)
+    }
+})
+server.listen(process.env.PORT||3000,()=>{
+    console.log('服务启动成功')
+})
